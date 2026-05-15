a351b6000b9c2090e11332c641811c95e1f1070a
jnealey88 <129418666+jnealey88@users.noreply.github.com>
2026-05-14 21:49:33 -0700

    feat(block): swap dashicon for custom DSGo SVG icon

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '4730908320fdc6993452');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'dab784e8ae1339489266');
